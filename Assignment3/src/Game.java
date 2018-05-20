be34acaa571a269d5b229688350251d215f56891
Falati <Falati@github.com>
2018-05-20 16:38:30 -0400

    Merge branch 'master' of https://github.com/bigcat2014/Artificial_Intelligence
    # Conflicts:
    #	Assignment3/src/Game.java

--- Assignment3/src/Game.java
+++ Assignment3/src/Game.java
@@ -40,4 +40,7 @@ public class Game {
     private boolean invalidForwardMove(int x1, int x2, int y1, int y2){
         return false;
     }
+    private boolean isPieceExistent(int x1, int x2, int y1, int y2){
+        return false;
+    }
 }
