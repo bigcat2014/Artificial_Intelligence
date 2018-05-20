44ed62fecb7bbf89021920b83d33239c9525afec
Falati <Falati@github.com>
2018-05-20 16:13:31 -0400

    Merge branch 'master' of https://github.com/bigcat2014/Artificial_Intelligence
    # Conflicts:
    #	Assignment3/src/Game.java

--- Assignment3/src/Game.java
+++ Assignment3/src/Game.java
@@ -4,7 +4,19 @@ public class Game {
     public static final int BOARD_SIZE = 8;
 
     public void printBoard() {
-        
+        for (int row = 0; row < BOARD_SIZE; row++)
+        {
+            System.out.println("");
+            System.out.println("---------------------------------");
+
+            for (int column = 0; column < BOARD_SIZE; column++)
+            {
+                System.out.print("- " + " " + " ");
+            }
+            System.out.print("-");
+        }
+        System.out.println("");
+        System.out.println("---------------------------------");
     }
 
     public boolean isIllegalMove(Move currentMove) {
